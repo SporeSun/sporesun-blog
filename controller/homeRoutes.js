c25678e6aafb9e0a577c4c85b5742248fb81a7ae
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-12 19:48:44 -0800

    added homeroutes and stylesheets

---
+++ controller/homeRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// Route to serve the homepage
+router.get('/', (req, res) => {
+    // Logic to render the homepage
+    // res.render('home', { /* data to pass to the template */ });
+});
+
+// Add more routes as needed
+
+module.exports = router;
