595bc98bd7080d3e51e76b1ea69e491625023d51
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-12 19:58:21 -0800

    added login/logout functionality, registration functionality, and altered dashboard.handlebars

---
+++ public/js/register.js
@@ -0,0 +1,20 @@
+document.getElementById('register-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('/api/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to register');
+    }
+});
